c995c49536075ab4915bb70216ee3dc29becfb3e
Documenter.jl <documenter@juliadocs.github.io>
2026-09-19 15:48:32 +0000

    build based on 94941e8

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"category":"section","location":"#BiometricTables","page":"Home","text":"Documentation for BiometricTables.\n\n","title":"BiometricTables"}]
+[{"category":"section","location":"#BiometricTables","page":"Home","text":"Documentation for BiometricTables.\n\n","title":"BiometricTables"},{"category":"method","location":"#BiometricTables.survival-Tuple{BiometricTables.MortalityTable, Int64, Int64}","page":"Home","text":"survival(mt::MortalityTable, age::Int, time::Int)::Float64\n\nTBW\n\n\n\n\n\n","title":"BiometricTables.survival"}]
 }
