94941e86dfb9dcbd9682efec0566fc5423f74b59
thfreud <thfreud@gmail.com>
2026-09-19 12:47:05 -0300

    Funções de probabilidades básicas implementadas

--- src/BiometricTables.jl
+++ src/BiometricTables.jl
@@ -1,4 +1,7 @@
 module BiometricTables
 
+export px, survival, ages, minimum_age, maximum_age
+
+include("Interface.jl")
 
 end
--- src/Interface.jl
+++ src/Interface.jl
@@ -1,7 +1,37 @@
-# interface de acesso aos campos
+# interface  para MortalityTable
 include("Types.jl")
 
+@inline function _age_index(mt::MortalityTable, age::Int)
+    idx = age - minimum_age(mt) + 1
+    @boundscheck checkbounds(mt.rates, idx)
+    return idx
+end
+
 ages(mt::MortalityTable) = mt.ages
+
 minimum_age(mt::MortalityTable) = minimum(ages(mt))
+
 maximum_age(mt::MortalityTable) = maximum(ages(mt))
+
 metadata(mt::MortalityTable) = mt.metadata
+
+function qx(mt::MortalityTable, age::Int)::Float64
+    idx = _age_index(mt, age)
+    return @inbounds mt.rates[idx]
+end
+
+px(mt::MortalityTable, age::Int)::Float64 = 1.0 - qx(mt, age)
+
+
+"""
+    survival(mt::MortalityTable, age::Int, time::Int)::Float64
+
+TBW
+"""
+function survival(mt::MortalityTable, age::Int, time::Int)::Float64
+    p = 1.0
+    for t_age in 0:(time-1)
+        p *= px(mt, age + t_age)
+    end
+    return p
+end
